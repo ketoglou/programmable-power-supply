16f5d6283a61f728c0787839a7630d6447a097b5
ketoglou <ketoglou@hotmail.com>
2019-05-17 00:46:56 +0300

    5V PS

---
+++ Firmware/5V_PPS_Project.X/UART1.h
@@ -0,0 +1,20 @@
+/*USART HEADER FILE
+ * Use USART_ReceiveByte for receive byte and  USART_SendString for send bytes
+ * if USART is not use receive or transmit functions static must be removed from the one that is not used
+ */
+#define RECEIVE_BUFFER_SIZE 32
+#define TRANSMIT_BUFFER_SIZE 55
+
+char rx_counter;
+char rx_buffer[RECEIVE_BUFFER_SIZE];
+char tx_buffer[TRANSMIT_BUFFER_SIZE];
+char tx_byte;
+unsigned char COMMAND_WR; //Determines if command is write(0) or read(1)
+unsigned char COMMAND; //Determines type of command
+int COMMAND_WRITE_NUMBER; //Determines the number for write commands
+
+
+void UART1_Init(unsigned char baud_rate);
+unsigned char UART1_SendByte(unsigned char byte);
+unsigned char UART1_SendString(char *str,int size);
+unsigned char UART1_ReceiveCommand(void);
---
+++ Software/ps_app.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+import time
+import serial
+import serial.tools.list_ports
+import threading
+import time
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class VoltageCurrent_thread(threading.Thread):
+    	
+	def __init__(self,ser,voltage_label,current_label):
+		threading.Thread.__init__(self)
+		self.t = threading.currentThread()
+		self.ser = ser
+		self.voltage_label = voltage_label
+		self.current_label = current_label
+		self.run()
+
+	def run(self):
+		while getattr(self.t, "do_run", True) and self.ser != None:
+			voltage = self.ser.send_serial("R0")
+			current_voltage = self.ser.send_serial("R1")
+			if voltage != None and current_voltage != None:
+				self.voltage_label.set_label(voltage)
+				current_voltage = float(current_voltage) / 4.9 #We divide with 4.9 because thats the factor the amplifier amplifies the voltage
+				current = current_voltage / 1000.0 		#We divide the real voltage drop we measure from amplifier with the resistor
+				current = current*5000.0				#The real current at load is the current we find above in the resistor*5000 as the datasheet of LT3081 says
+				self.current_label.set_label("{0:.6f}".format(current))
+			time.sleep(1)
+
+class SerialPort:
+
+	def find_ports(self):
+		results = serial.tools.list_ports.comports()
+		if len(results) == 0:
+			return []
+		ports = []
+		if type(results[0]) == tuple:
+			for result in results:
+				ports.append(result[0])
+		else:
+			for result in results:
+				ports.append(result.device)
+		return ports
+
+	def open_serial(self,s_port):
+		try:
+			self.ser = serial.Serial( port=s_port, baudrate=19200, bytesize=serial.EIGHTBITS, 	parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE, timeout=1, xonxoff=False, rtscts=False, write_timeout=None, dsrdtr=False, inter_byte_timeout=None, exclusive=None)
+			self.ser.isOpen()
+			self.serialOpen = True
+		except:
+			print("Cannot connect with the device.")
+
+	def close_serial(self):
+		try:
+			self.serialOpen = False
+			self.ser.close()
+		except:
+			pass
+
+	def send_serial(self,command):
+		try:
+			if self.send_status == False :
+				self.send_status = True
+				self.ser.write(str.encode(command + "\r\n"))
+				if command == "R2" :
+					respond = self.ser.readline().decode("utf-8") + self.ser.readline().decode("utf-8") + self.ser.readline().decode("utf-8")
+				else:
+					respond = self.ser.readline().decode("utf-8")
+				self.send_status = False
+				return respond
+			else:
+				return None
+		except:
+			print("Cannot connect with the device.")
+			self.info_label.set_label("Cannot connect!\nTry to change the port\nor replug the device.")
+
+	def __init__(self,info_label):
+		self.serialOpen = False
+		self.ser = None
+		self.send_status = False
+		self.info_label = info_label
+
+
+class UIWindow:
+
+	def on_window_destroy(self, object, data=None):
+		if self.thread == None :
+			pass
+		elif self.thread.isAlive() :
+			self.thread.do_run = False
+			self.thread.join()
+		self.serial.close_serial()
+		Gtk.main_quit()
+	
+	def combo_box_change(self,widget,data=None):
+		try:
+			if widget.get_active_text() != "" and self.refresh == False:
+				print("Serial open:"+widget.get_active_text())
+				if self.thread == None:
+					pass
+				elif self.thread.isAlive() :
+					self.thread.do_run = False
+					self.thread.join()
+				self.serial.close_serial()
+				self.serial.open_serial(widget.get_active_text())
+				ver = self.serial.send_serial("R2")
+				if ver[0:7] == "Version" :
+					self.info_label.set_label(ver)
+					self.connection_status = True
+					self.thread = threading.Thread(target=VoltageCurrent_thread,args=(self.serial,self.voltage_label,self.current_label))
+					self.thread.start()
+				else:
+					self.info_label.set_label("Cannot connect!\nTry to change the port\nor replug the device.")
+					self.serial.close_serial()
+					self.connection_status = False
+					self.voltage_label.set_label("")
+					self.current_label.set_label("")
+					self.message_label.set_label("")
+			else:
+				self.refresh = False
+		except:
+			print("Cannot connect with the device")
+			self.info_label.set_label("Cannot connect!\nTry to change the port\nor replug the device.")
+
+	def refresh_ports(self,widget,data=None):
+		self.ports = self.serial.find_ports()
+		self.refresh = True
+		self.combo_box.remove_all()
+		for port in self.ports:
+			self.combo_box.append_text(port)
+
+	def led_on(self,widget,data=None):
+		if self.connection_status == True :
+			tries = 10
+			while self.serial.send_serial("W00001") == None and --tries:
+				pass
+			if tries > 0 :
+				self.message_label.set_label("LED is on.")
+
+	def led_off(self,widget,data=None):
+		if self.connection_status == True :
+			tries = 10
+			while self.serial.send_serial("W00000") == None and --tries:
+				pass
+			if tries > 0 :
+				self.message_label.set_label("LED is off.")
+
+	def voltage_set(self,widget,data=None):
+		try:
+			voltage = float(self.voltage_set_val.get_text())
+			if voltage >= 0.0 and voltage <= 5.0 :
+				r = voltage / 0.00005 #According to LT3081 datasheet with this method we calculate the resistor
+				ad5272_step = 100000.0 / 1023.0 #The AD5272 step
+				r = int(round(r / ad5272_step))
+				#self.serial.send_serial("W1"+str(int(round(r))))
+				if r < 10 :
+					self.serial.send_serial("W1000"+str(r))
+					print("Serial send:W1000"+str(r))
+				elif r < 100:
+					self.serial.send_serial("W100"+str(r))
+					print("Serial send:W100"+str(r))
+				elif r < 1000:
+					self.serial.send_serial("W10"+str(r))
+					print("Serial send:W10"+str(r))
+				else:
+					self.serial.send_serial("W1"+str(r))	
+					print("Serial send:W1"+str(r))
+				self.message_label.set_text("Voltage Set!")
+			else:
+				self.message_label.set_text("The range is from 0V-5V!")	
+		except:
+			self.message_label.set_text("Use only numbers!")
+
+	def current_set(self,widget,data=None):	
+		pass
+
+	def __init__(self):
+		self.refresh = False
+		self.connection_status = False
+		self.thread = None
+		self.gladefile = "ui.glade" 
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file(self.gladefile)
+		self.builder.connect_signals(self)
+		self.window = self.builder.get_object("GtkWindow")
+		self.combo_box = self.builder.get_object("combo_box")
+		self.info_label = self.builder.get_object("info_label")
+		self.voltage_label = self.builder.get_object("voltage_value_label")
+		self.current_label = self.builder.get_object("current_value_label")
+		self.voltage_set_val = self.builder.get_object("voltage_input")
+		self.current_set_val = self.builder.get_object("current_input")
+		self.message_label = self.builder.get_object("message_label")
+		self.serial = SerialPort(self.info_label)
+		self.ports = self.serial.find_ports()
+		for port in self.ports:
+			self.combo_box.append_text(port)
+
+		self.window.show()
+
+
+#MAIN
+if __name__ == "__main__":
+	try:
+		ui_window = UIWindow()
+		Gtk.main()
+	except KeyboardInterrupt:
+		pass
